ed43a275b43266dfc5e93d7736f6a85b50e2bd7f
唐二伟 <james.tang@aihuishou.com>
2019-02-13 18:26:58 +0800

    订单详情调通用户信息，机器属性部分

--- src/containers/order/components/deliverSatus.tsx
+++ src/containers/order/components/deliverSatus.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { IOrderProps, IDeliverSatus } from '@/containers/order/interface/order.inerface';
 import UPSICON from '@/images/order/upsIcon.png';
 import './deliverSatus.less';
+import { Modal } from 'antd';
 
 class DeliverSatus extends React.Component<IOrderProps, IDeliverSatus> {
     constructor(props: IOrderProps) {
@@ -11,11 +12,21 @@ class DeliverSatus extends React.Component<IOrderProps, IDeliverSatus> {
             visible: false
         }
     }
+    public showModal = () => {
+        this.setState({
+            visible: true,
+        });
+    }
+    public closeModal = () => {
+        this.setState({
+            visible: false,
+        });
+    }
     public render() {
         return (
             <div className="comp-order-deliverSatus">
                 <p className="title">Delivery Status</p>
-                <div className="body">
+                <div className="deliverSatus-body">
                     <div className="col-1">
                         <div className="list-item">
                             <div className="date">Jan 14</div>
@@ -61,10 +72,63 @@ class DeliverSatus extends React.Component<IOrderProps, IDeliverSatus> {
                     </div>
                 </div>
                 <div className="footer">
-                    <div>Check Detail ></div>
+                    <div onClick={this.showModal}>Check Detail ></div>
                 </div>
-
-            </div>
+                <Modal
+                    visible={this.state.visible}
+                    title=""
+                    footer={null}
+                    onCancel={this.closeModal}
+                    width={800}
+                    wrapClassName="antd-modal-deliverSatus-body"
+                >
+                    <div className="antd-modal-deliverSatus-body">
+                        <div className="col-2">
+                            <div>
+                                <img src={UPSICON} alt="" />
+                                <div>
+                                    <p className="name">UPS</p>
+                                    <p className="tel">XY1303918832FR</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-1">
+                            <div className="list-item">
+                                <div className="date">Jan 14</div>
+                                <div className="date-list">
+                                    <div className="date-item">
+                                        <div className="time">01:32 PM</div>
+                                        <div className="desc">
+                                            <p className="info">A mechanical failure has delayed. We’re adjusting plans to deliver your package as quickly as possible.</p>
+                                            <p className="pos">Paris, France</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="line" />
+                            <div className="list-item">
+                                <div className="date">Jan 14</div>
+                                <div className="date-list">
+                                    <div className="date-item">
+                                        <div className="time">01:32 PM</div>
+                                        <div className="desc">
+                                            <p className="info">A mechanical failure has delayed. We’re adjusting plans to deliver your package as quickly as possible.</p>
+                                            <p className="pos">Paris, France</p>
+                                        </div>
+                                    </div>
+                                    <div className="date-item">
+                                        <div className="time">01:32 PM</div>
+                                        <div className="desc">
+                                            <p className="info">A mechanical failure has delayed. We’re adjusting plans to deliver your package as quickly as possible.</p>
+                                            <p className="pos">Paris, France</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Modal>
+            </div >
         );
     }
 }
--- src/containers/order/index.tsx
+++ src/containers/order/index.tsx
@@ -1,24 +1,44 @@
 import * as React from 'react';
 import { inject, observer } from 'mobx-react';
-import { IOrderProps } from '@/containers/order/interface/order.inerface';
-// import OrderPlaced from "@/containers/order/orderPlaced";
-// import PackageSent from '@/containers/order/packageSent';
-// import EL from '@/containers/order/packageReceived';
-// import EL from '@/containers/order/InspectionCompleted';
-// import EL from '@/containers/order/listForSale';
-import EL from '@/containers/order/orderCompleted';
+import { IOrderProps, IProgressType } from '@/containers/order/interface/order.inerface';
+import ProgressBar from '@/containers/order/components/progressBar';
+import MachineInfo from '@/containers/order/components/machineInfo';
+import DeliverSatus from '@/containers/order/components/deliverSatus';
+import UserInfo from '@/containers/order/components/userInfo';
 import './index.less';
 
 @inject("order")
 @observer
 class Order extends React.Component<IOrderProps> {
   public componentDidMount() {
-    this.props.order.getOrderDetail();
+    this.props.order.getOrderDetail("1001");
   }
   public render() {
+    // 根据订单状态展示不同页面
+    const alreadyLoadData = !!this.props.order.orderDetail.orderNo;
+    // 组件展示对象
+    const ReactNodeConfig = {
+      deliver: false
+    }
+    // 是否展示物流(已发货未收货，已收货未质检)
+    if ((this.props.order.orderDetail.status === IProgressType.TO_BE_RECEIVED) || (this.props.order.orderDetail.status === IProgressType.TO_BE_INSPECTED)) {
+      ReactNodeConfig.deliver = true;
+    }
     return (
       <div className="page-order-container">
-        <EL {...this.props} />
+        <section>
+          <ProgressBar data={this.props.order.progressType} />
+          {ReactNodeConfig.deliver && <DeliverSatus {...this.props} />}
+          {
+            alreadyLoadData && (<>
+              <p>Order Summary</p>
+              <div className="info-container">
+                <MachineInfo {...this.props.order.machineInfo} />
+                <UserInfo {...this.props.order.userInformation} />
+              </div>
+            </>)
+          }
+        </section>
       </div>
     )
   }
