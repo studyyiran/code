d68e6dca1b588bd5a9a6058a1549c295d38aa46c
殷煌 <huang.yin@aihuishou.com>
2019-02-14 17:51:55 +0800

    update

--- scripts/build.js
+++ scripts/build.js
@@ -4,7 +4,7 @@
 process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
 if (process.env.REACT_APP_SERVER_ENV === 'UAT') {
-  process.env.PUBLIC_URL = 'static-www-staging-uptradeit.aihuishou.com';
+  process.env.PUBLIC_URL = 'https://static-www-staging-uptradeit.aihuishou.com';
 }
 // if(process.env.REACT_APP_SERVER_ENV === 'PUB') {
 //   process.env.PUBLIC_URL = 'https://static.bangmaibao.net';
