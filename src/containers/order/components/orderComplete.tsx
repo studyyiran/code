05d4869e3f30da11c192a537b80849e2324c209d
唐二伟 <james.tang@aihuishou.com>
2019-02-15 11:46:49 +0800

    add paid state

--- src/containers/order/components/orderComplete.tsx
+++ src/containers/order/components/orderComplete.tsx
@@ -1,36 +1,48 @@
 import * as React from 'react';
+import { Icon } from 'antd';
 import { IOrderProps } from '@/containers/order/interface/order.inerface';
 import Tag from '@/components/tag';
 import './orderComplete.less';
 
 class ListedForSale extends React.Component<IOrderProps> {
     public render() {
-        // const tag = {
-        //     type: "fail",
-        //     text: "Wrong Condition"
-        // }
         const tag = {
-            type: "success",
-            text: "Matched"
+            type: "fail",
+            text: "Wrong Condition"
         }
+        // const tag = {
+        //     type: "success",
+        //     text: "Matched"
+        // }
         return (
-            <div className="comp-order-lisedForSale">
+            <div className="comp-order-orderComplete">
                 <div className="auction-result table">
                     <p>Auction Result</p>
                     <div>
                         <div>Final Sale Price</div>
                         <div>$660</div>
-                        <section className="table-table">
-                            <div>
-                                <div>Price Guarantee <span>Paid</span></div>
+                        <div className="flex">
+                            <div className="col">
+                                <div>
+                                    <span>Price Guarantee</span>
+                                    <span className="paid">
+                                        Paid
+                                        <Icon className="paid-check" type="check" />
+                                    </span>
+                                </div>
                                 <div>$610</div>
                             </div>
-                            <div>
-                                <div>Bonus <span>Paid</span></div>
+                            <div className="col">
+                                <div>
+                                    <span>Bonus</span>
+                                    <span className="paid">
+                                        Paid
+                                        <Icon className="paid-check" type="check" />
+                                    </span>
+                                </div>
                                 <div>$50</div>
                             </div>
-                        </section>
-
+                        </div>
                     </div>
                 </div>
                 <div className="inspection-success-result table">
@@ -38,10 +50,18 @@ class ListedForSale extends React.Component<IOrderProps> {
                         <span>Inspection Result</span>
                         <Tag className="inspect-title-tag" {...tag} />
                     </p>
-                    <div>
+                    {/* match */}
+                    {tag.type === "success" && (<div>
                         <div>Price Guarantee</div>
                         <div>$710 </div>
-                    </div>
+                    </div>)}
+                    {/* wrong condition */}
+                    {tag.type === "fail" && (<div>
+                        <div>Revised Price Guarantee</div>
+                        <div style={{ color: "#FF5858" }}>$710 </div>
+                        <div>Difference</div>
+                        <div style={{ height: "auto" }}>iPhone Xs 64G Power Off,CracksiPhone Xs 64G Power Off,CracksiPhone Xs 64G Power Off,CracksiPhone Xs 64G Power Off,CracksiPhone Xs 64G Power Off,CracksiPhone Xs 64G Power Off,CracksiPhone Xs 64G Power Off,CracksiPhone Xs 64G Power Off,CracksiPhone Xs 64G Power Off,CracksiPhone Xs 64G Power Off,CracksiPhone Xs 64G Power Off,Cracks </div>
+                    </div>)}
                 </div>
             </div>
         );
