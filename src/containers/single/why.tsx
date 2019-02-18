29a84cf466391d09c2b01ba75328737c57849806
殷煌 <huang.yin@aihuishou.com>
2019-02-18 14:13:29 +0800

    update

--- src/containers/single/why.tsx
+++ src/containers/single/why.tsx
@@ -1,7 +1,19 @@
 import * as React from 'react';
-
+import { Button } from 'antd';
+import './why.less';
 export default class Why extends React.Component {
   public render() {
-    return 123;
+    return (
+      <div className="page-single-why-container">
+        <div className="banner">
+          <div className="left">
+            <h1>We Reinvented<br /> The Way <br />You Sell Your Phone</h1>
+            <p>Top reasons why we are not your ordinary trade in program.</p>
+            <Button type="primary" style={{ width: 166 }} size="large" >SEZLL IT NOW</Button>
+          </div>
+          <img src={require('@/images/single/why_banner.png')} height="100%" alt="" className="right" />
+        </div>
+      </div>
+    )
   }
 }
