f2104e180f812fe9d75dfb014479e45aedef6017
殷煌 <huang.yin@aihuishou.com>
2019-02-25 14:08:45 +0800

    update

--- src/containers/layout/header.tsx
+++ src/containers/layout/header.tsx
@@ -12,7 +12,7 @@ export default class Header extends React.Component {
           <Link to={'/sell-my-phone'}><span className="how_it_works_btn">How it Works</span></Link>
           <Link to={'/who-we-are'}><span className="about_us_btn">About Us</span></Link>
           <Link to={'/order/check'}><span className="check_your_order_btn">Check Your Order</span></Link>
-          <Link to={'/sell/account'}><Button type="primary">Sell Your Phone Now</Button></Link>
+          <Link to={'/sell/account'}><Button type="primary">SELL YOUR PHONE NOW</Button></Link>
         </div>
         <div className="header-bg" />
       </div>
