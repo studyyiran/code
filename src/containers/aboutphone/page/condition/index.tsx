86b70066e1500565c712a0fdff136e530cd6e23f
yiranzi <570932296@qq.com>
2019-08-17 07:25:55 +0800

    review: 修改

--- src/containers/aboutphone/page/condition/index.tsx
+++ src/containers/aboutphone/page/condition/index.tsx
@@ -5,6 +5,7 @@ import {IAction} from '@/interface/index.interface'
 import {IQuestion, IUserAnswer, IUserQuestionAnswer} from './index.interface'
 import {WrapperPanel} from './components/wrapperPanel'
 import {isCanMove, isNoContinue} from './util'
+import {serverPhoneInfoQuestion, serverQuestionArr} from './mock'
 
 /*
 default 和 active似乎 遵从active
@@ -108,204 +109,27 @@ function reducer(state: any, action: IAction) {
 export default function () {
   return <Conditions />
 }
-
+// questionnaire
 // 后端接口
 interface IConditions {
-  phoneInfo?: any,
-  phoneConditionArr?: any,
+  phoneInfoQuestion?: IQuestion[],
+  phoneConditionQuestion?: IQuestion[],
+  userPhoneConditionAnswer?: IUserQuestionAnswer[],
+  userPhoneInfoAnswer?: IUserQuestionAnswer[],
 }
 
 export function Conditions(props: IConditions) {
-  const {phoneInfo = [
-  {
-    id: 'phoneInfoSubQuestion1',
-    subAnswerArr: [
-      {
-        id: 'phoneInfoSubQuestion1',
-        answer: ['1']
-      },
-      {
-        id: 'phoneInfoSubQuestion2',
-        answer: ['1']
-      },
-      {
-        id: 'phoneInfoSubQuestion3',
-        answer: ['1']
-      },
-      {
-        id: 'phoneInfoSubQuestion4',
-        answer: ['1']
-      }
-    ]
-  },
-    {
-      id: 'phoneInfoSubQuestion2',
-      subAnswerArr: [
-        {
-          id: 'phoneInfoSubQuestion1',
-          answer: ['1']
-        },
-        {
-          id: 'phoneInfoSubQuestion2',
-          answer: ['1']
-        },
-        {
-          id: 'phoneInfoSubQuestion3',
-          answer: ['1']
-        },
-        {
-          id: 'phoneInfoSubQuestion4',
-          answer: ['1']
-        }
-      ]
-    },
-    {
-      id: 'phoneInfoSubQuestion3',
-      subAnswerArr: [
-        {
-          id: 'phoneInfoSubQuestion1',
-          answer: ['1']
-        },
-        {
-          id: 'phoneInfoSubQuestion2',
-          answer: ['1']
-        },
-        {
-          id: 'phoneInfoSubQuestion3',
-          answer: ['1']
-        },
-        {
-          id: 'phoneInfoSubQuestion4',
-          answer: ['1']
-        }
-      ]
-    },
-    {
-      id: 'phoneInfoSubQuestion4',
-      subAnswerArr: [
-        {
-          id: 'phoneInfoSubQuestion1',
-          answer: ['1']
-        },
-        {
-          id: 'phoneInfoSubQuestion2',
-          answer: ['1']
-        },
-        {
-          id: 'phoneInfoSubQuestion3',
-          answer: ['1']
-        },
-        {
-          id: 'phoneInfoSubQuestion4',
-          answer: ['1']
-        }
-      ]
-    }
-  ], phoneConditionArr = []} = props
+  const {
+    phoneInfoQuestion = serverPhoneInfoQuestion || [],
+    phoneConditionQuestion = serverQuestionArr || [],
+    userPhoneConditionAnswer = []} = props
   const initState = {
     userPhoneInfoInput: phoneInfo,
     userAnswerInput: phoneConditionArr, // 用户输入的。
-    editKey: [] // 用户输入的。
+    editKey: [] 
   };
-  const questionArr = [
-    {
-      id: "1",
-      title: "Power On",
-      subQuestionArr: [
-        {
-          id: "10",
-          content: "Does your phone power on to the home screen?",
-          type: 'default',
-        }
-      ]
-    },
-    {
-      id: "2",
-      title: "Scratch",
-      subQuestionArr: [
-        {
-          id: "20",
-          content: "Are there any scratches on the phone?",
-          isMoreCondition: ["true"],
-          type: 'default',
-        },
-        {
-          id: "21",
-          content: "Are there scratches on the screen itself?",
-          isMoreCondition: ["true"],
-          type: 'default',
-        },
-      ]
-    },
-    {
-      id: "3",
-      title: "Crack",
-      subQuestionArr: [
-        {
-          id: "30",
-          content: "Are there any cracks on your phone?",
-          isMoreCondition: ["true"],
-          type: 'default',
-        },
-        {
-          id: "31",
-          content: "Where is the crack located? (Select all that apply)",
-          type: 'multiSelect',
-          questionDesc: ['Screen', 'Back Cover', 'Back Camera']
-        }
-      ]
-    },
-    {
-      id: "4",
-      title: "Functionality",
-      subQuestionArr: [
-        {
-          id: "41",
-          content: "Is your phone fully functional?",
-          isMoreCondition: ["true"],
-          type: 'default',
-        },
-        {
-          id: "42",
-          content: "Which of the following aren't working? (Select all that apply)",
-          isMoreCondition: ["true"],
-          type: 'multiSelect',
-          questionDesc: ['Charger', 'Touch ID', 'Home Button']
-        },
-      ],
-    },
-    {
-      id: "5",
-      title: "Internet Accounts",
-      subQuestionArr: [
-        {
-          id: "51",
-          content: "Has iCloud or Google account been removed from your phone?",
-          isMoreCondition: ["true"],
-          type: 'default',
-        },
-        {
-          id: "52",
-          content: "We can't accept locked phone. Can you remove before sending in?",
-          isMoreCondition: ["true"],
-          type: 'default',
-        },
-      ]
-    },
-    {
-      id: "6",
-      title: "Payoff Status",
-      subQuestionArr: [
-        {
-          id: "61",
-          content: "Is your phone fully paid off?",
-          type: 'default',
-        },
-      ]
-    },
-  ]
   const [state, dispatch] = useReducer(reducer, initState);
-  return <ConditionForm state={state} dispatch={dispatch} questionArr={questionArr} />;
+  return <ConditionForm state={state} dispatch={dispatch} questionArr={serverQuestionArr} />;
 }
 
 
@@ -315,37 +139,7 @@ interface IConditionForm {
   questionArr: IQuestion[],
 }
 const firstQuestionKey = "aboutYourPhone"
-// 关于手机情况的 写死的 问题
-const phoneInfoQuestion : IQuestion = {
-  id: firstQuestionKey,
-  title: 'About your phone',
-  subQuestionArr: [
-    {
-      id: 'phoneInfoSubQuestion1',
-      content: 'Phone Manufacture',
-      questionDesc: ['Brand1', 'Brand2'],
-      type: 'select',
-    },
-    {
-      id: 'phoneInfoSubQuestion2',
-      content: 'Model',
-      questionDesc: ['iPhone X', 'iPhone 8'],
-      type: 'select',
-    },
-    {
-      id: 'phoneInfoSubQuestion3',
-      content: 'Storage',
-      questionDesc: ['64GB', '128GB'],
-      type: 'select',
-    },
-    {
-      id: 'phoneInfoSubQuestion4',
-      content: 'Carrier',
-      questionDesc: ['Carrier1', 'Carrier2'],
-      type: 'select',
-    }
-  ]
-}
+
 
 export function ConditionForm(props: IConditionForm) {
   const [maxActiveKey, setMaxActiveKey] = useState("")
@@ -487,206 +281,3 @@ export function ConditionForm(props: IConditionForm) {
       </div>
   );
 }
-
-// import { IConditionProps } from './index.interface';
-// import { inject, observer } from 'mobx-react';
-
-
-// import Layout from '@/containers/aboutphone/layout';
-// import ConditionItem from '@/containers/aboutphone/components/conditionitem';
-
-// import { IConditionsProps, ISubSkuPricePropertyValues } from './interface/index.interface';
-// import { message } from 'antd';
-// import { IProductInfo } from '@/store/interface/user.interface';
-// import { conditionPageValidate, noteUserModal } from '@/containers/aboutphone/pageValidate';
-// import Breadcrumb from '@/containers/aboutphone/components/breadcrumb';
-// import ProgressBar from '@/containers/aboutphone/components/progressbar--mobile';
-// import classnames from 'classnames';
-
-// const TBDPPNS = [
-//   { "id": 316, "name": "Can your phone power on to the Home screen?", "isSkuProperty": false, "pricePropertyValues": [{ "id": 2026, "propertyName": 316, "value": "Turns on", "isPreferred": true, "isSkuProperty": false }, { "id": 2027, "propertyName": 316, "value": "Doesn't turn on", "isPreferred": false, "isSkuProperty": false }], "illustrationContent": null },
-//   { "id": 345, "name": "Is your phone 100% functional?", "isSkuProperty": false, "pricePropertyValues": [{ "id": 2104, "propertyName": 345, "value": "Fully functional", "isPreferred": true, "isSkuProperty": false }, { "id": 2105, "propertyName": 345, "value": "Have functional problems", "isPreferred": false, "isSkuProperty": false }], "illustrationContent": null },
-//   { "id": 351, "name": "Is your display cracked?", "isSkuProperty": false, "pricePropertyValues": [{ "id": 2118, "propertyName": 351, "value": "No Cracks", "isPreferred": true, "isSkuProperty": false }, { "id": 2122, "propertyName": 351, "value": "Cracked", "isPreferred": false, "isSkuProperty": false }], "illustrationContent": null }
-// ]
-
-// @inject('yourphone')
-// @observer
-// export default class Index extends React.Component<IConditionProps> {
-//   public render() {
-//     return <Collapse>
-//      
-//       <Panel header="This is panel header 2" key="2">
-//         <p>{2}</p>
-//       </Panel>
-//       <Panel header="This is panel header 3" key="3">
-//         <p>{3}</p>
-//       </Panel>
-//     </Collapse>
-//     return <div className="page-condition">
-//       <div className="comp-process"></div>
-//       <h1 className="condition">Phone condition details</h1>
-//       <Collapse>
-//         <Panel>
-//           <span className="icon"></span>
-//           <span className="title"></span>
-//           <span className="click">Edit Step null</span>
-//         </Panel>
-//         <div className="question" onChangeHendler={true}>
-//           <select>
-//            
-//           </select>
-//         </div>
-//       </Collapse>
-//     </div>
-//   }
-// }
-
-// @inject('yourphone', 'user', 'common')
-// @observer
-// export default class Index extends React.Component<IConditionsProps> {
-//   public state = {
-//     progress: 3,
-//     disabled: true
-//   }
-//   public async componentDidMount() {
-//     // 显示左侧价格模块
-//     this.props.user.isShowLeftPrice = true;
-//     if (!conditionPageValidate()) {
-//       this.props.history.push('/sell/yourphone/brand');
-//       return;
-//     }
-//     // tbd 赛默认选项
-//     if (this.props.yourphone.isTBD) {
-//       this.props.yourphone.productPPVNS = TBDPPNS;
-//     } else {
-//       // 非 tbd 调用正常的询价项
-//       await this.props.yourphone.getProductPPVN();
-//     }
-//
-//     // 初次进入页面判断是否要高亮
-//     if (this.props.yourphone.isAllConditionSelected) {
-//       this.setState({
-//         progress: 4,
-//         // disabled: false
-//       })
-//     }
-//
-//     // done页面，允许父组件调用里面的方法
-//     if (typeof this.props.onRef === 'function') {
-//       this.props.onRef!(this);
-//     }
-//   }
-//
-//   public validateData = (): Promise<boolean> => {
-//     return new Promise(async (resolve) => {
-//       const yourphone = this.props.yourphone
-//
-//       if (!yourphone.isAllConditionSelected) {
-//         message.info('Please make sure you have chosen all of items.');
-//         resolve(false);
-//       }
-//
-//       const isInquiryKeyCreated = await this.props.yourphone.createInquiry();
-//       resolve(isInquiryKeyCreated);
-//     });
-//   }
-//
-//   public render() {
-//     const conditionList: React.ReactNode = (
-//       this.props.yourphone.productPPVNS.map((ppvn, index) => (
-//         <ConditionItem
-//           key={index}
-//           {...ppvn}
-//           activeConditions={this.props.yourphone.activeConditions}
-//           onConditionItemClick={this.onConditionItemClick}
-//         />
-//       ))
-//     );
-//
-//     return (
-//       <div className={classnames('page-conditions-container', { 'notlayout': this.props.hideLayout })}>
-//         {
-//           !this.props.hideLayout
-//             ? (
-//               <Layout nextCb={this.handleNext} progress={this.state.progress} disabled={!this.props.yourphone.isAllConditionSelected}>
-//                 <>
-//                   {
-//                     this.props.common.isMobile && <ProgressBar />
-//                   }
-//                   <Breadcrumb
-//                     brandName={this.props.yourphone.activeBrandsName}
-//                     carrierName={this.props.yourphone.activeCarrierDescription}
-//                     modelName={`${this.props.yourphone.activeProductName} ${this.props.yourphone.activeModelName}`}
-//                   />
-//                   {conditionList}
-//                 </>
-//               </Layout>
-//             )
-//             : (conditionList)
-//         }
-//       </div>
-//     )
-//   }
-//
-//   private onConditionItemClick = (conditionId: number, ppvnValueId: number) => {
-//     this.props.yourphone.activeConditions = { ...this.props.yourphone.activeConditions, [conditionId]: ppvnValueId };
-//     if (this.props.yourphone.isAllConditionSelected) {
-//       this.setState({
-//         progress: 4,
-//         // disabled: false
-//       })
-//     }
-//   }
-//
-//   private handleNext = async () => {
-//     if (this.props.yourphone.isTBD) {
-//       this.props.yourphone.tbdInfo.properties = [];
-//       Object.keys(this.props.yourphone.activeConditions).forEach((key: string) => {
-//         const ppn = TBDPPNS.find(v => v.id.toString() === key);
-//         let ppv: ISubSkuPricePropertyValues | null = null;
-//         if (ppn) {
-//           ppv = ppn.pricePropertyValues.find(v => v.id === this.props.yourphone.activeConditions[key]) || null;
-//         }
-//         if (ppv) {
-//           this.props.yourphone.tbdInfo.properties.push(ppv.value);
-//         }
-//       })
-//       this.props.history.push('/sell/yourphone/shipping');
-//       return;
-//     }
-//     const isInquiryKeyCreated = await this.validateData();
-//
-//     if (isInquiryKeyCreated) {
-//       try {
-//         const productInfo: Partial<IProductInfo> = {
-//           ...this.props.user.preOrder.productInfo,
-//           priceUnits: [...Object.values(this.props.yourphone.activeConditions), this.props.yourphone.activeModelId],
-//         }
-//         this.props.user.preOrder = {
-//           ...this.props.user.preOrder,
-//           inquiryKey: this.props.yourphone.inquiryKey,
-//           productInfo
-//         };
-//       } catch (error) { }
-//
-//       let path = '/sell/yourphone/shipping'
-//       if (this.props.user.preOrder.appendOrderDetail) {
-//         path = '/sell/yourphone/done'
-//       }
-//       // 询价成功，提示用户，有保证金的存在，只存在于PC，因为移动端价格面板在最上面啦
-//       if (!this.props.common.isMobile) {
-//         noteUserModal({
-//           title: 'Your UpTrade Offer',
-//           content: (<>Your {this.props.yourphone.inquiryDetail!.product.name} Guaranteed Price is ${this.props.yourphone.inquiryDetail!.priceDollar} <br /> <br />This window will be closed after 15 seconds.</>),
-//           type: 'success',
-//           seconds: 15,
-//           update: (seconds) => (<>Your {this.props.yourphone.inquiryDetail!.product.name} Guaranteed Price is ${this.props.yourphone.inquiryDetail!.priceDollar} <br /> <br />This window will be closed after {seconds} seconds.</>),
-//           customerOk: () => this.props.history.push(path)
-//         });
-//       } else {
-//         this.props.history.push(path);
-//       }
-//
-//     }
-//   }
-// }
---
+++ src/containers/aboutphone/page/condition/mock.ts
@@ -0,0 +1,217 @@
+import {IQuestion, IUserQuestionAnswer} from './index.interface'
+
+export const serverPhoneInfo : IUserQuestionAnswer[] = [
+  {
+    id: 'phoneInfoSubQuestion1',
+    subAnswerArr: [
+      {
+        id: 'phoneInfoSubQuestion1',
+        answer: ['1']
+      },
+      {
+        id: 'phoneInfoSubQuestion2',
+        answer: ['1']
+      },
+      {
+        id: 'phoneInfoSubQuestion3',
+        answer: ['1']
+      },
+      {
+        id: 'phoneInfoSubQuestion4',
+        answer: ['1']
+      }
+    ]
+  },
+  {
+    id: 'phoneInfoSubQuestion2',
+    subAnswerArr: [
+      {
+        id: 'phoneInfoSubQuestion1',
+        answer: ['1']
+      },
+      {
+        id: 'phoneInfoSubQuestion2',
+        answer: ['1']
+      },
+      {
+        id: 'phoneInfoSubQuestion3',
+        answer: ['1']
+      },
+      {
+        id: 'phoneInfoSubQuestion4',
+        answer: ['1']
+      }
+    ]
+  },
+  {
+    id: 'phoneInfoSubQuestion3',
+    subAnswerArr: [
+      {
+        id: 'phoneInfoSubQuestion1',
+        answer: ['1']
+      },
+      {
+        id: 'phoneInfoSubQuestion2',
+        answer: ['1']
+      },
+      {
+        id: 'phoneInfoSubQuestion3',
+        answer: ['1']
+      },
+      {
+        id: 'phoneInfoSubQuestion4',
+        answer: ['1']
+      }
+    ]
+  },
+  {
+    id: 'phoneInfoSubQuestion4',
+    subAnswerArr: [
+      {
+        id: 'phoneInfoSubQuestion1',
+        answer: ['1']
+      },
+      {
+        id: 'phoneInfoSubQuestion2',
+        answer: ['1']
+      },
+      {
+        id: 'phoneInfoSubQuestion3',
+        answer: ['1']
+      },
+      {
+        id: 'phoneInfoSubQuestion4',
+        answer: ['1']
+      }
+    ]
+  }
+]
+
+export const serverQuestionArr : IQuestion[] = [
+  {
+    id: "1",
+    title: "Power On",
+    subQuestionArr: [
+      {
+        id: "10",
+        content: "Does your phone power on to the home screen?",
+        type: 'default',
+      }
+    ]
+  },
+  {
+    id: "2",
+    title: "Scratch",
+    subQuestionArr: [
+      {
+        id: "20",
+        content: "Are there any scratches on the phone?",
+        isMoreCondition: ["true"],
+        type: 'default',
+      },
+      {
+        id: "21",
+        content: "Are there scratches on the screen itself?",
+        isMoreCondition: ["true"],
+        type: 'default',
+      },
+    ]
+  },
+  {
+    id: "3",
+    title: "Crack",
+    subQuestionArr: [
+      {
+        id: "30",
+        content: "Are there any cracks on your phone?",
+        isMoreCondition: ["true"],
+        type: 'default',
+      },
+      {
+        id: "31",
+        content: "Where is the crack located? (Select all that apply)",
+        type: 'multiSelect',
+        questionDesc: ['Screen', 'Back Cover', 'Back Camera']
+      }
+    ]
+  },
+  {
+    id: "4",
+    title: "Functionality",
+    subQuestionArr: [
+      {
+        id: "41",
+        content: "Is your phone fully functional?",
+        isMoreCondition: ["true"],
+        type: 'default',
+      },
+      {
+        id: "42",
+        content: "Which of the following aren't working? (Select all that apply)",
+        isMoreCondition: ["true"],
+        type: 'multiSelect',
+        questionDesc: ['Charger', 'Touch ID', 'Home Button']
+      },
+    ],
+  },
+  {
+    id: "5",
+    title: "Internet Accounts",
+    subQuestionArr: [
+      {
+        id: "51",
+        content: "Has iCloud or Google account been removed from your phone?",
+        isMoreCondition: ["true"],
+        type: 'default',
+      },
+      {
+        id: "52",
+        content: "We can't accept locked phone. Can you remove before sending in?",
+        isMoreCondition: ["true"],
+        type: 'default',
+      },
+    ]
+  },
+  {
+    id: "6",
+    title: "Payoff Status",
+    subQuestionArr: [
+      {
+        id: "61",
+        content: "Is your phone fully paid off?",
+        type: 'default',
+      },
+    ]
+  },
+]
+// 关于手机情况的 写死的 问题
+export const serverPhoneInfoQuestion : IQuestion = {
+  id: firstQuestionKey,
+  title: 'About your phone',
+  subQuestionArr: [
+    {
+      id: 'phoneInfoSubQuestion1',
+      content: 'Phone Manufacture',
+      questionDesc: ['Brand1', 'Brand2'],
+      type: 'select',
+    },
+    {
+      id: 'phoneInfoSubQuestion2',
+      content: 'Model',
+      questionDesc: ['iPhone X', 'iPhone 8'],
+      type: 'select',
+    },
+    {
+      id: 'phoneInfoSubQuestion3',
+      content: 'Storage',
+      questionDesc: ['64GB', '128GB'],
+      type: 'select',
+    },
+    {
+      id: 'phoneInfoSubQuestion4',
+      content: 'Carrier',
+      questionDesc: ['Carrier1', 'Carrier2'],
+      type: 'select',
+    }
+  ]
+}
