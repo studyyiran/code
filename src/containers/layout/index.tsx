720f9b8493dc1923c340bf4c659aca0b4892b2c7
yiran_aihuishou <yiran.sun@aihuishou.com>
2019-08-26 18:20:06 +0800

    update: footer-mb-style

--- src/containers/layout/index.tsx
+++ src/containers/layout/index.tsx
@@ -1,19 +1,19 @@
-import * as React from 'react';
-import config from '../../config';
-import * as PropTypes from 'prop-types';
-import './index.less';
-import HeaderHoc from './headerHoc'
-import FooterHoc from './footerHoc'
+import * as React from "react";
+import config from "../../config";
+import * as PropTypes from "prop-types";
+import "./index.less";
+import HeaderHoc from "./headerHoc";
+import FooterHoc from "./footerHoc";
 // import { getQueryString } from 'utils';
-import commonStore from '@/store/common';
-import Raven from 'raven-js';
+import commonStore from "@/store/common";
+import Raven from "raven-js";
 export default class LayoutIndex extends React.Component {
   // 通过context 拿到 router 对象
   public static contextTypes = {
     router: PropTypes.shape({
       history: PropTypes.object.isRequired
     }).isRequired
-  }
+  };
   // public readonly state = {
   //   showMobileFooter: true
   // }
@@ -29,7 +29,7 @@ export default class LayoutIndex extends React.Component {
     // }
 
     // message.loading(ECommonText.LOADING, 1);
-    window['__history__'] = this.context.router.history;
+    window["__history__"] = this.context.router.history;
     // 获取title 配置 以及拿到所有的title key
     const titles = config.TITLE_CONFIG;
     const titlesKey = Object.keys(titles);
@@ -52,28 +52,32 @@ export default class LayoutIndex extends React.Component {
   }
 
   public componentDidCatch(error: any, errorInfo: any) {
-    if (process.env.REACT_APP_SERVER_ENV === 'PUB') {
+    if (process.env.REACT_APP_SERVER_ENV === "PUB") {
       Raven.captureException(error, { extra: errorInfo });
     }
   }
 
   public onMappingTitles = (titlesKey: string[], titles: object) => {
-
     // 得到所有和当前路由匹配的数组
-    const arr = titlesKey.filter(v => new RegExp(v).test(this.context.router.history.location.pathname));
+    const arr = titlesKey.filter(v =>
+      new RegExp(v).test(this.context.router.history.location.pathname)
+    );
     // 设置title
     const currentConfig = titles[arr[arr.length - 1]];
-    document.title = currentConfig ? currentConfig.title : '';
-    document.querySelector('meta[name="keywords"]')!.setAttribute('content', currentConfig ? currentConfig.keywords : '');
-    document.querySelector('meta[name="description"]')!.setAttribute('content', currentConfig ? currentConfig.description : '');
-
-  }
+    document.title = currentConfig ? currentConfig.title : "";
+    document
+      .querySelector('meta[name="keywords"]')!
+      .setAttribute("content", currentConfig ? currentConfig.keywords : "");
+    document
+      .querySelector('meta[name="description"]')!
+      .setAttribute("content", currentConfig ? currentConfig.description : "");
+  };
 
   public render() {
     return (
       <div className="layout-container">
         <HeaderHoc router={this.context.router} />
-        {this.props.children}
+        <main>{this.props.children}</main>
         <FooterHoc router={this.context.router} />
       </div>
     );
