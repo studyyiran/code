c322c620f50d50b7daa4613f99b9e4aea974c05e
yiran_aihuishou <yiran.sun@aihuishou.com>
2019-08-26 11:38:10 +0800

    update: review-comp-pc-style

--- src/containers/home/components/review/index.tsx
+++ src/containers/home/components/review/index.tsx
@@ -2,27 +2,17 @@ import * as React from 'react';
 import { observer } from 'mobx-react'
 import Star from '@/components/star';
 import { IReview } from './index.interface';
-import { Icon } from 'antd';
 import './index.less';
-{/*<article className="comp-review-card">*/}
-{/*  <div className="review-card__rate">1 2 3</div>*/}
-{/*  <p>"is good"</p>*/}
-{/*  <div className="review-card__footer">*/}
-{/*    <span>sun yiran</span>*/}
-{/*    <span>icon</span>*/}
-{/*  </div>*/}
-{/*</article>*/}
-export default observer((props: IReview) => {
+const ReviewItem = ((props: IReview) => {
   return <div className="comp-review-card">
-    <div className="top">
-      <div className="reviews-rating-list">
-        <Star size="small" gray={true} rate={Number(props.rating)} />
-      </div>
-    </div>
-    <p className="content">{props.comments}</p>
-    <div className="review-card__footer">
-      <span>{props.reviewer.first_name} {props.reviewer.last_name}</span>
-      <Icon type="check-circle" />
-    </div>
+    <header>
+      <Star size="small" gray={true} rate={Number(props.rating)} />
+      <span className="date">{props.reviewer.last_name}</span>
+    </header>
+    <p>{props.comments}</p>
+    <footer className="review-card__footer">
+      <span>{props.reviewer.first_name}</span>
+    </footer>
   </div>
 })
+export default ReviewItem
