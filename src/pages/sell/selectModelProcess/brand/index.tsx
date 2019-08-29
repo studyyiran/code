8179e307585dbc8f6529491feca7547ae53c1949
yiran_aihuishou <yiran.sun@aihuishou.com>
2019-08-29 18:27:56 +0800

    update: brand页面

--- src/pages/sell/selectModelProcess/brand/index.tsx
+++ src/pages/sell/selectModelProcess/brand/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useContext } from "react";
 import sellServer from "../server";
 import { SelectModelContext, ISelectModelContext } from "../context";
+import { HeaderTitle } from "@/components/headerTitle";
 
 // interface IBrand {}
 
@@ -14,27 +15,34 @@ export default function Brand(props: any) {
   } = brandContext as ISelectModelContext;
   const { brandList } = selectModelContextValue;
   useEffect(() => {
-    console.log('dispatch')
-    dispatch({type: 'setCategoryId', value: '1'})
+    console.log("dispatch");
+    dispatch({ type: "setCategoryId", value: "1" });
   }, []);
   function selectBrandHandler(id: string) {
-    dispatch({type: 'setBrand', value: id});
+    dispatch({ type: "setBrand", value: id });
     // props.canGoNext() && props.goNextPage();
   }
   function renderList() {
     return brandList.map((item: any) => {
-      const {name, id} = item
+      const { name, id } = item;
       return (
-        <div
-          key={id}
-          onClick={() => {
-            selectBrandHandler(id);
-          }}
-        >
-          {name}
-        </div>
+        <li className={}>
+          <div
+            key={id}
+            onClick={() => {
+              selectBrandHandler(id);
+            }}
+          >
+            {name}
+          </div>
+        </li>
       );
-    })
+    });
   }
-  return <div>{renderList()}</div>;
+  return (
+    <div className="brand">
+      <HeaderTitle title={"Select a manufacturer"} />
+      <ul className="barnd-list">{renderList()}</ul>
+    </div>
+  );
 }
