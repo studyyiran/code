5eaf61dbdff2cfee9143a7411661ebcda48d3fbc
yiran_aihuishou <yiran.sun@aihuishou.com>
2019-08-29 17:53:35 +0800

    update: router

--- src/pages/sell/index.tsx
+++ src/pages/sell/index.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import Brand from "./selectModelProcess/brand";
-import { ModelContextProvider } from "./selectModelProcess/context";
+import {Switch, Route} from 'react-router'
 
+// const config = [
+//   {
+//     url: 'sell'
+//   },
+//   {
+//     url: ''
+//   }
+// ]
 // interface ISell {}
 export default function Sell(props: any) {
   console.log(props);
@@ -12,17 +20,45 @@ export default function Sell(props: any) {
     console.log("goNextPage");
   }
 
-  function wrapper(children: any) {
-    return (...other: any[]) => {
-      return React.cloneElement(children, {
+  // function wrapper(Component: any) {
+  //   return (...other: any[]) => {
+  //     const NewA = React.cloneElement(Component, {
+  //       canGoNext,
+  //       goNextPage,
+  //       ...props,
+  //       ...other
+  //     })
+  //     // return <Component />
+  //     // @ts-ignore
+  //     console.log(<NewA />)
+  //     console.log(Component)
+  //     console.log('end')
+  //     // @ts-ignore
+  //     return <NewA />;
+  //   };
+  // }
+  function wrapperWithProps(Component: any) {
+    return (other: any[]) => {
+      const newProps = {
         canGoNext,
         goNextPage,
         ...props,
         ...other
-      });
-    };
+      }
+      return <Component {...newProps} />
+    }
   }
-  return <ModelContextProvider>
-    <Brand />
-  </ModelContextProvider>;
+  
+  const Model = () => {
+    return <div>123</div>
+  }
+  return <Switch>
+    <Route path={props.match.url + "/"} component={wrapperWithProps(Brand)} />
+    <Route path={props.match.url + "/:brandName"} component={wrapperWithProps(Model)} />
+    <Route path={props.match.url + "/:brandName/:model"} component={wrapperWithProps(Model)} />
+    <Route render={() => <div>123</div>} />
+  </Switch>
+  // return <ModelContextProvider>
+  //  
+  // </ModelContextProvider>;
 }
