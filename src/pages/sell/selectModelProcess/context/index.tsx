74e923be1e78381aa261264f64bb6d0e098f369e
yiran_aihuishou <yiran.sun@aihuishou.com>
2019-08-30 22:05:20 +0800

    update: cache

--- src/pages/sell/selectModelProcess/context/index.tsx
+++ src/pages/sell/selectModelProcess/context/index.tsx
@@ -7,44 +7,57 @@ import React, {
 import { IReducerAction } from "@/interface/index.interface";
 import { getBrandsByCid, getProductsList } from "../server/index.api";
 
+let haveLoad = false;
+const sessionKey = "modelContext";
+
 export const SelectModelContext = createContext({});
 
 function reducer(state: IContextState, action: IReducerAction) {
   const { type, value } = action;
+  let newState;
   switch (type) {
     case "setProductsList": {
-      return {
+      newState = {
         ...state,
         productsList: value
       };
+      break;
     }
     case "setCategoryId": {
-      return {
+      newState = {
         ...state,
         categoryId: value
       };
+      break;
     }
     case "setBrandList": {
-      return {
+      newState = {
         ...state,
         brandList: value
       };
+      break;
     }
     case "setBrand": {
-      return {
+      newState = {
         ...state,
         brand: value
       };
+      break;
     }
     case "setModelInfo": {
-      return {
+      newState = {
         ...state,
         modelInfo: value
       };
+      break;
     }
     default:
-      return { ...state };
+      newState = { ...state };
   }
+  debugger
+  console.log("run reducer");
+  saveToCache(sessionKey, state, ["modelInfo", "brand", "categoryId"]);
+  return newState;
 }
 // const promisify = (func: (...param: any[]) => void) => (...args: any[]) => {
 //   return Promise.resolve(func(...args));
@@ -74,14 +87,20 @@ function useGetAction(
     }),
     getProductsList: promisify(async function() {
       if (state.categoryId && state.brand) {
-        const productsList = await getProductsList(state.brand, state.categoryId);
-        console.log(productsList)
+        const productsList = await getProductsList(
+          state.brand,
+          state.categoryId
+        );
+        console.log(productsList);
         dispatch({ type: "setProductsList", value: productsList });
       }
     })
   };
   actions.getBrandList = useCallback(actions.getBrandList, [state.categoryId]);
-  actions.getProductsList = useCallback(actions.getProductsList, [state.brand, state.categoryId]);
+  actions.getProductsList = useCallback(actions.getProductsList, [
+    state.brand,
+    state.categoryId
+  ]);
   return actions;
 }
 
@@ -105,17 +124,24 @@ export interface ISelectModelContext extends IContextActions {
 }
 
 export function ModelContextProvider(props: any) {
-  const initState: IContextState = {
+  let initState: IContextState = {
     brandList: [],
     modelInfo: {
-      modelId: '',
-      storageId: '',
-      carrierId: '',
+      modelId: "",
+      storageId: "",
+      carrierId: ""
     },
     productsList: [],
     categoryId: "",
     brand: ""
   };
+  if (!haveLoad) {
+    haveLoad = true;
+    const cache = reloadFromCache(sessionKey);
+    if (cache) {
+      initState = { ...initState, ...cache };
+    }
+  }
   const [state, dispatch] = useReducer(reducer, initState);
   const action: IContextActions = useGetAction(state, dispatch);
   // id变化，重新拉brand
@@ -125,11 +151,28 @@ export function ModelContextProvider(props: any) {
   // brand变化，重新拉机型
   useEffect(() => {
     action.getProductsList();
-  }, [action.getProductsList])
+  }, [action.getProductsList]);
   const propsValue: ISelectModelContext = {
     ...action,
     selectModelContextValue: state,
     dispatch
   };
   return <SelectModelContext.Provider value={propsValue} {...props} />;
+}
+
+function saveToCache(key: string, value: any, needKey: any[]) {
+  const cache = {};
+  needKey.forEach(item => {
+    cache[item] = value[item];
+  });
+  sessionStorage.setItem(key, JSON.stringify(cache));
+}
+
+function reloadFromCache(key: string) {
+  const data = sessionStorage.getItem(key);
+  if (data) {
+    return JSON.parse(data);
+  } else {
+    return null;
+  }
 }
