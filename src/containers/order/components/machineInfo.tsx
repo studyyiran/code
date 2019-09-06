5be9c65c8edb8f6d8b83bd682595c1f32032a1b5
yiran_aihuishou <yiran.sun@aihuishou.com>
2019-09-06 15:48:56 +0800

    update: 修改common-style

--- src/containers/order/components/machineInfo.tsx
+++ src/containers/order/components/machineInfo.tsx
@@ -8,33 +8,30 @@ class MachineInfo extends React.Component<
   public render() {
     return (
       <div className="comp-order-machineInfo">
-        <div className="properties">
-          <ul className="information-list">
+        <ul className="information-list">
+          <li>
+            <span>Model</span>
+            <span>{this.props.productName}</span>
+          </li>
+          {this.props.carrier !== "" && (
             <li>
-              <span>Model</span>
-              <span>{this.props.productName}</span>
+              <span>Carrier</span>
+              <span>{this.props.carrier}</span>
             </li>
-            {this.props.carrier !== "" && (
-              <li>
-                <span>Carrier</span>
-                <span>{this.props.carrier}</span>
-              </li>
-            )}
-          </ul>
-          <div className="line" />
-          <p className="total">
-            <span>subTotal</span>
-            <span>{this.props.guaranteedPrice}</span>
-          </p>
-          {/*{*/}
-          {/*    this.props.condition !== "" && (*/}
-          {/*        <div>*/}
-          {/*            <div>Condition</div>*/}
-          {/*            <div>{this.props.condition}</div>*/}
-          {/*        </div>*/}
-          {/*    )*/}
-          {/*}*/}
-        </div>
+          )}
+        </ul>
+        <p className="total">
+          <span>subTotal</span>
+          <span>{this.props.guaranteedPrice}</span>
+        </p>
+        {/*{*/}
+        {/*    this.props.condition !== "" && (*/}
+        {/*        <div>*/}
+        {/*            <div>Condition</div>*/}
+        {/*            <div>{this.props.condition}</div>*/}
+        {/*        </div>*/}
+        {/*    )*/}
+        {/*}*/}
       </div>
     );
   }
