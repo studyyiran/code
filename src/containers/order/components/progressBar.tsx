e50ecc00bc28df426b3e7996234d55aa08d344de
yiranzi <570932296@qq.com>
2019-09-07 11:12:37 +0800

    update: 修改progress isMobile

--- src/containers/order/components/progressBar.tsx
+++ src/containers/order/components/progressBar.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { IProgressData } from "@/containers/order/interface/order.inerface";
 import "./progressBar.less";
+import {inject, observer} from "mobx-react";
 
 interface IDot {
   isActive: boolean;
@@ -26,30 +27,47 @@ class Dot extends React.Component<IDot> {
   }
 }
 
-class ProgressBar extends React.Component<{ data: IProgressData }> {
+@inject("common")
+@observer
+class ProgressBar extends React.Component<{ data: IProgressData, common?: any }> {
   public render() {
+    const { isMobile } = this.props.common;
     const props = this.props.data;
     const cls = ["comp-order-progressBar"];
-    if (props.currentIndex > 3) {
-      cls.push("mobileHideLeft");
+    //
+    let showList = props.dataList;
+    if (isMobile) {
+      const count = 3;
+      let start;
+      if (props.currentIndex === 0) {
+        start = 0;
+      } else if (props.currentIndex === props.dataList.length) {
+        start = props.dataList.length - 3;
+      } else {
+        start = props.currentIndex - 1;
+      }
+      showList = props.dataList.slice(start, start + count);
     }
     return (
       <div className={cls.join(" ")}>
-        {props.dataList.map((t, i) => (
-          <Dot
-            isActive={true}
-            key={i}
-            isCurrent={i === props.currentIndex}
-          >
-            {i === props.currentIndex && t.img && (
-              <div className="icon">
-                <img src={t.img} alt="" />
-              </div>
-            )}
-            <div className="title">{t.name}</div>
-            {t.date && <div className="date">{t.date}</div>}
-          </Dot>
-        ))}
+        {showList.map((t, i) => {
+          const isCurrent = t.name === props.dataList[props.currentIndex].name;
+          return (
+            <Dot
+              isActive={true}
+              key={i}
+              isCurrent={isCurrent}
+            >
+              {isCurrent && t.img && (
+                <div className="icon">
+                  <img src={t.img} alt="" />
+                </div>
+              )}
+              <div className="title">{t.name}</div>
+              {t.date && <div className="date">{t.date}</div>}
+            </Dot>
+          );
+        })}
       </div>
     );
   }
