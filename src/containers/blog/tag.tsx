d73107afc155907213e3baf2ac7371f9c1217717
yiran_aihuishou <yiran.sun@aihuishou.com>
2019-09-20 16:10:14 +0800

    ff-new: 增加页面

--- src/containers/blog/tag.tsx
+++ src/containers/blog/tag.tsx
@@ -1,30 +1,30 @@
-import * as React from 'react';
-import { observer, inject } from 'mobx-react';
-import { Link } from 'react-router-dom';
-import { Button } from 'antd'
-import { IBlogListProps, IBlog } from './interface/blog.interface';
-import './list.less';
-import * as moment from 'moment-timezone';
-import { shareComponent } from '@/utils/function'
-@inject('blog')
+import * as React from "react";
+import { observer, inject } from "mobx-react";
+import { IBlogListProps, IBlog } from "./interface/blog.interface";
+import "./list.less";
+import "./common.less";
+import { shareComponent } from "@/utils/function";
+import Blog from "@/containers/blog/components/blogCard";
+@inject("blog")
 @observer
 export default class BlogList extends React.Component<IBlogListProps> {
   public componentDidMount() {
-    if (window['__SERVER_RENDER__INITIALSTATE__']) {
-      const initialState = window['__SERVER_RENDER__INITIALSTATE__'];
-      this.props.blog.tagPageListPagination = initialState.blog.tagPageListPagination;
+    if (window["__SERVER_RENDER__INITIALSTATE__"]) {
+      const initialState = window["__SERVER_RENDER__INITIALSTATE__"];
+      this.props.blog.tagPageListPagination =
+        initialState.blog.tagPageListPagination;
       this.props.blog.tagPageList = initialState.blog.tagPageList;
       this.props.blog.viewTagMore = initialState.blog.viewTagMore;
-      window['__SERVER_RENDER__INITIALSTATE__'] = null;
+      window["__SERVER_RENDER__INITIALSTATE__"] = null;
     } else {
       this.props.blog.tagPageListPagination = {
         tagSlug: this.props.match.params.tag,
         pageIndex: 0,
         pageSize: 10
-      }
+      };
       this.props.blog.getTagPageList();
       try {
-        window.scrollTo(0, 0)
+        window.scrollTo(0, 0);
       } catch (e) {
         console.warn(e);
       }
@@ -39,44 +39,35 @@ export default class BlogList extends React.Component<IBlogListProps> {
   public render() {
     const blog = this.props.blog;
     return (
-      <div className="page-blog-list-container">
-        <div className="bloglist-list-wrapper">
+      <div className="blog-list-part-container">
+        <section className="blog-list-part">
           <header>{blog.currentTag}</header>
-          <div className="list-box">
-            {
-              blog.tagPageList.map((item: IBlog, index: number) => {
-                return (
-                  <div className="list" key={index}>
-                    <Link to={'/' + item.slug}>
-                      <div className="img-box">
-                        <div className="img" style={{ backgroundImage: `url(${item.thumbnailFullUrl})` }} />
-                        <img src={item.thumbnailFullUrl} alt={item.thumbnailFullUrl + " | UpTradeit.com"} />
-                      </div>
-                      <div className="right">
-                        <h3>{item.title}</h3>
-                        <small>{moment.tz(item.releaseDt, "America/Chicago").format('MMM DD, YYYY HHA')}</small>
-                        <p>{item.summary}</p>
-                      </div>
-                    </Link>
-                  </div>
-                )
-              })
-            }
-          </div>
-          {
-            this.props.blog.viewTagMore && (
-              <div className="button-group">
-                <Button type="primary" ghost={true} size="large" className="view-more" onClick={this.handleMore}>VIEW MORE</Button>
-              </div>
-            )
-          }
-        </div>
+          <nav>
+            {blog.tagPageList.map(props => {
+              return <Blog {...props} key={props.title} />;
+            })}
+          </nav>
+        </section>
+        {/*{this.props.blog.viewTagMore && (*/}
+        {/*  <div className="button-group">*/}
+        {/*    <Button*/}
+        {/*      type="primary"*/}
+        {/*      ghost={true}*/}
+        {/*      size="large"*/}
+        {/*      className="view-more"*/}
+        {/*      onClick={this.handleMore}*/}
+        {/*    >*/}
+        {/*      VIEW MORE*/}
+        {/*    </Button>*/}
+        {/*  </div>*/}
+        {/*)}*/}
       </div>
-    )
+    );
   }
 
   private handleMore = () => {
-    this.props.blog.tagPageListPagination.pageIndex = this.props.blog.tagPageListPagination.pageIndex + 1;
+    this.props.blog.tagPageListPagination.pageIndex =
+      this.props.blog.tagPageListPagination.pageIndex + 1;
     this.props.blog.getTagPageList();
-  }
+  };
 }
