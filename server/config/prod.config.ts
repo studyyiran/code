eab79153a5cad794d5f8ebe2df7f622918d3f112
yiran_aihuishou <yiran.sun@aihuishou.com>
2019-09-29 18:53:00 +0800

    update: 修改ssr

--- server/config/prod.config.ts
+++ server/config/prod.config.ts
@@ -1,5 +1,5 @@
 export default {
   port: 3006,
   proxyUrl: "http://10.114.0.22/",
-  routerIgnore: ["/check-order", "/order", "/sell"]
+  routerIgnore: ["/check-order", "/order", "/sell-phone"]
 };
