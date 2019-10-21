bd835ca1b35d654900b42665ed8bcd3ff2e4c8c1
codemperor <emper_zhang@aihuishou.com>
2019-10-21 21:36:56 +0800

    修改qa脚本

--- devstack/pm2-deploy.sh
+++ devstack/pm2-deploy.sh
@@ -2,6 +2,6 @@
 
 echo "pm2 start"
 
-pm2 stop sell
-pm2 delete sell
-pm2 start /home/ec2-user/uptrade-home/build/compiled.js --name sell
+pm2 stop uptrade
+pm2 delete uptrade
+pm2 start /home/ec2-user/uptrade-home/build/compiled.js --name uptrade
--- devstack/qa-build.sh
+++ devstack/qa-build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 APP_WORKSPACE="$(pwd)"
+BUILD=$1
 
-echo "APP_WORKSPACE: ${APP_WORKSPACE}"
+echo "APP_WORKSPACE: ${APP_WORKSPACE}, BUILD: ${BUILD}"
 
 /home/ec2-user/node-v8.11.4-linux-x64/bin/yarn -v
 /home/ec2-user/node-v8.11.4-linux-x64/bin/yarn install
-/home/ec2-user/node-v8.11.4-linux-x64/bin/yarn run pm2:uat
+/home/ec2-user/node-v8.11.4-linux-x64/bin/yarn run ${BUILD}
 
 cd build
 rm -rf build.tar.gz
