308f41c192fedb5d66a217f94267d4dfa294b1ce
yiran_aihuishou <yiran.sun@aihuishou.com>
2019-10-24 14:26:21 +0800

    update: 修复首页

--- src/buy/pages/home/ssr.tsx
+++ src/buy/pages/home/ssr.tsx
@@ -58,8 +58,8 @@ export const ourHomeSsrRule = async (url: string) => {
   }
 
   const buyProductList: any = await getBuyProductList({
-    seq: sellListTitle[0].seqNo,
-    brandId: sellListTitle[0].brandId
+    seq: buyListTitle[0].seqNo,
+    brandId: buyListTitle[0].brandId
   });
   if (buyProductList && buyProductList.length) {
     store.storeData.buyProductList = buyProductList;
