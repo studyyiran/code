232b25aa9c2740ecea58e75e4c5ce37180a94f87
codemperor <emper_zhang@aihuishou.com>
2019-10-24 11:15:30 +0800

    优化发布脚本

--- devstack/pm2-deploy.sh
+++ devstack/pm2-deploy.sh
@@ -3,5 +3,6 @@
 ENV=$1
 echo "pm2 start, BUILD: ${ENV}"
 
+pm2 flush #暂时暴力清理所有日志， 否则日志无限大，影响性能，后续加入pm2-logrotate做日志管理
 pm2 stop all
 pm2 start /home/ec2-user/uptrade-home/pm2-start.json --env=${ENV}
