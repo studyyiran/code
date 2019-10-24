ef8383e257a99804d6005b613d3910c86c73c729
yiran_aihuishou <yiran.sun@aihuishou.com>
2019-10-24 21:21:38 +0800

    update: 修改老链接跳转

--- src/containers/notfound/index.tsx
+++ src/containers/notfound/index.tsx
@@ -20,7 +20,7 @@ class NotFound extends React.Component {
           <h1>Apologies… something isn’t right here</h1>
           <p>This can’t be what you were looking for. <br />There may be a technical issue that brought you here, or you may be trying to access a page that doesn’t exist.</p>
           <div className="common-button">
-            <Button type="primary" size="large"><Link to="/sell">GO BACK TO HOME</Link></Button>
+            <Button type="primary" size="large"><a href="/">GO BACK TO HOME</a></Button>
           </div>
           <p>If your issue persists, please email support@uptradeit.com</p>
         </div>
