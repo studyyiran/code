08fbc988503e2522156e52310591d88e7b76e059
yiran_aihuishou <yiran.sun@aihuishou.com>
2019-11-14 10:33:34 +0800

    .

--- src/buy/common/constValue/index.tsx
+++ src/buy/common/constValue/index.tsx
@@ -1,6 +1,7 @@
 export const constValue = {
   REFUNDTIME: 30,
   priceUnit: "$",
+  AUTHKEY: 'uptrade_us_frontend_super_fuck_token',
   fiveActValue: 1.4,
   paypalButtonId: "paypal-button-container"
 }
