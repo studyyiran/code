d39afe1e55fa67ff7e013e919dde09cdbe5303b4
yiran_aihuishou <yiran.sun@aihuishou.com>
2019-11-21 14:56:56 +0800

    add

---
+++ src/containers/order/container/components/showFeePrice/index.tsx
@@ -0,0 +1,25 @@
+import React, { useContext } from "react";
+import './index.less'
+import { ITotalOrderInfoContext, TotalOrderInfoContext } from "../../context";
+import {currencyTrans} from "../../../../../utils/util";
+
+export function ShowFeePrice() {
+  // 监听
+  const totalOrderInfoContext = useContext(TotalOrderInfoContext);
+  // 获取
+  const {
+    totalOrderInfoContextValue
+  } = totalOrderInfoContext as ITotalOrderInfoContext;
+  // 获取
+  const { totalOrderInfo } = totalOrderInfoContextValue;
+  const { chargedInsurance, insuranceFee } = totalOrderInfo as any;
+  if (!chargedInsurance && insuranceFee) {
+    return (
+      <p className="fee-red-tips">
+        *Your shipping insurance {currencyTrans(insuranceFee)} will be charged at your 1st
+        payout
+      </p>
+    );
+  }
+  return null;
+}
