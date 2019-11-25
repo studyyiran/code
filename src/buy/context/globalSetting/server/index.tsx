1a919464855b59de81f6b08cc404da2727b52864
yiran_aihuishou <yiran.sun@aihuishou.com>
2019-11-25 13:47:33 +0800

    修改倒计时时间

--- src/buy/context/globalSetting/server/index.tsx
+++ src/buy/context/globalSetting/server/index.tsx
@@ -9,6 +9,6 @@ export const getBlackFiveTimeUrl = "/auth/countdown/time";
 export async function getBlackFiveTime() {
   const res: any = await ajax.get(getBlackFiveTimeUrl);
   // return 1036859578;
-  // return 4000;
+  // return 1 * 60 * 60 * 1000  + 1000 * 60;
   return res;
 }
