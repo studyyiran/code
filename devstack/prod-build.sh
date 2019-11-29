d351c89d858751d43042680d274c534283ff3572
yiran_aihuishou <yiran.sun@aihuishou.com>
2019-11-29 13:32:07 +0800

    v0.10.3

--- devstack/prod-build.sh
+++ devstack/prod-build.sh
@@ -2,7 +2,7 @@
 
 APP_WORKSPACE="$(pwd)"
 S3_PATH=s3://prod-uptradeit-website/sell-website/build
-LAST_VERSION=${S3_PATH}-v0.10.2
+LAST_VERSION=${S3_PATH}-v0.10.3
 
 echo "APP_WORKSPACE: ${APP_WORKSPACE}, S3_PATH: ${S3_PATH}, LAST_VERSION: ${LAST_VERSION}"
 
