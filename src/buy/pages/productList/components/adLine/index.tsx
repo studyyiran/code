46302a85c6cacfad984d92ae5da184f8568a7080
yiran_aihuishou <yiran.sun@aihuishou.com>
2019-12-24 18:47:55 +0800

    继续测试渲染异常

--- src/buy/pages/productList/components/adLine/index.tsx
+++ src/buy/pages/productList/components/adLine/index.tsx
@@ -1,20 +1,22 @@
 import React from "react";
 import "./index.less";
 import { RenderByCondition } from "../../../../components/RenderByCondition";
-import {sellPageGoTo} from "../../../../common/utils/util";
-import {locationHref} from "../../../../common/utils/routerHistory";
+import { sellPageGoTo } from "../../../../common/utils/util";
+import { locationHref } from "../../../../common/utils/routerHistory";
 export default function AdLine(props: any) {
   const { line } = props;
   let dom;
   switch (String(line)) {
     case "0":
-      dom = (
-        <div className="ad-line">
+      return (
+        <div className="ad-line" key="adline0">
           <div>
             <h3>Questions?</h3>
             <p>We’ve got answers</p>
           </div>
-          <a className="big" onClick={() => locationHref("/contact")}>Get Help</a>
+          <a className="big" onClick={() => locationHref("/contact")}>
+            Get Help
+          </a>
           {/*<RenderByCondition*/}
           {/*  ComponentMb={<a className="big">972-833.0136</a>}*/}
           {/*  ComponentPc={<a className="big">Get Help</a>}*/}
@@ -23,8 +25,8 @@ export default function AdLine(props: any) {
       );
       break;
     case "1":
-      dom = (
-        <div className="ad-line">
+      return (
+        <div className="ad-line" key="adline1">
           <h3>Buy with confidence</h3>
           <RenderByCondition
             ComponentMb={null}
@@ -39,8 +41,8 @@ export default function AdLine(props: any) {
       );
       break;
     case "2":
-      dom = (
-        <div className="ad-line">
+      return (
+        <div className="ad-line" key="adline2">
           <h3>Protect your phone</h3>
           <RenderByCondition
             ComponentMb={null}
@@ -52,8 +54,8 @@ export default function AdLine(props: any) {
       );
       break;
     case "3":
-      dom = (
-        <div className="ad-line">
+      return (
+        <div className="ad-line" key="adline3">
           <h3>“Fast, professional, great price.”</h3>
           <p className="name">-Craig Black</p>
         </div>
@@ -62,5 +64,4 @@ export default function AdLine(props: any) {
     default:
       return null;
   }
-  return dom;
 }
