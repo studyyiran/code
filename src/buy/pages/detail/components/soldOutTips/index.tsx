6032b95a03e6a3a12d54833d1b0ddd68b2e2a721
yiran_aihuishou <yiran.sun@aihuishou.com>
2019-12-31 16:49:16 +0800

    upt-619: 新增提示售出组件

--- src/buy/pages/detail/components/soldOutTips/index.tsx
+++ src/buy/pages/detail/components/soldOutTips/index.tsx
@@ -1,29 +1,46 @@
 import React from "react";
 import Modal from "../../../../components/modal";
+import "./index.less";
 import { locationHref } from "../../../../common/utils/routerHistory";
-import { getLocationUrl } from "../../../../common/utils/util";
+import {
+  getLocationUrl,
+  getProductListPath
+} from "../../../../common/utils/util";
 import { viewAllClickHandler } from "../../util";
-import Button from "../../../../components/button";
 
 export function soldOutTips(detailInfo: any) {
   console.log(soldOutTips);
   (Modal as any).confirm({
-    width: "70%",
+    width: "90%",
+    className: "sold-out-tips-modal",
     closable: false,
     title: null,
-    footer: "single",
-    maskClosable: true,
-    cancelText: "Got it",
+    footer: null,
+    maskClosable: false,
     onCancel: () => {
       locationHref(getLocationUrl("home"));
     },
     children: (
       <div className="sold-out-tips">
-        <Button
-          onClick={() => {
-            viewAllClickHandler(detailInfo);
-          }}
-        >123</Button>
+        <h2>The product is sold out.</h2>
+        <div className="button-container">
+          <button
+            className="common-button"
+            onClick={() => {
+              viewAllClickHandler(detailInfo);
+            }}
+          >
+            Find similar phones
+          </button>
+          <span className="or">OR</span>
+          <a
+            onClick={() => {
+              window.location.href = getProductListPath();
+            }}
+          >
+            Back to category page
+          </a>
+        </div>
       </div>
     )
   });
