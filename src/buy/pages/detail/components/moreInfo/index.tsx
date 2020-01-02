3895f0ff579fb49f4ddddc4dbb433c48e6b3fa21
yiran_aihuishou <yiran.sun@aihuishou.com>
2020-01-02 16:43:24 +0800

    upt-613: 修改静态区域样式1

--- src/buy/pages/detail/components/moreInfo/index.tsx
+++ src/buy/pages/detail/components/moreInfo/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import "./index.less";
 import { isServer } from "../../../../common/utils/util";
 
 export function MoreInfo(): any {
@@ -18,21 +19,25 @@ export function MoreInfo(): any {
         "UpTrade Certified is our guarantee that the device listed for sale is like new in quality, fully functional, and not blacklisted. It starts with an industry leading inspection process. The 50+ point inspection checks for and ensures that every hardware and software feature on the phone works. We then sanitize and clean the phone before it is ready to be shipped to you."
     },
     {
-      icon: require("./res/uptrade-check.svg"),
+      icon: require("./res/phone.png"),
       title: "Our Story",
       content:
         "Our mission is to make today better so that the world will be brighter tomorrow; to build a company where the life of used electronics is extended through the hands of others or recycled to help reduce carbon footprint. We are committed to providing a fast, easy, safe, and trustworthy service for everyone."
     }
   ];
-  return content.map(({ icon, title, content }) => {
-    console.log(icon);
-    return (
-      <div key={title}>
-        {/*暂时强制更新 为了解决首次不正常渲染的问题*/}
-        <img src={icon} key={currentKey + icon} />
-        <h3 className="sub-title-size">{title}</h3>
-        <p>{content}</p>
-      </div>
-    );
-  });
+  return (
+    <div className="more-info-container">
+      {content.map(({ icon, title, content }) => {
+        console.log(icon);
+        return (
+          <div className="more-info" key={title}>
+            {/*暂时强制更新 为了解决首次不正常渲染的问题*/}
+            <img src={icon} key={currentKey + icon} />
+            <h3 className="sub-title-size">{title}</h3>
+            <p>{content}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
