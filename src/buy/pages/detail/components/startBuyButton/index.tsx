1cba712535f9c7efa55b33742c43ba5d6c6691e8
yiran_aihuishou <yiran.sun@aihuishou.com>
2020-01-03 16:48:50 +0800

    upt-613: 修改箭头

--- src/buy/pages/detail/components/startBuyButton/index.tsx
+++ src/buy/pages/detail/components/startBuyButton/index.tsx
@@ -18,7 +18,7 @@ export function StartBuyButton(props: {
       </Button>
       <span className="or">OR</span>
       <a className="link" onClick={viewAllClickHandler.bind({}, productDetail)}>
-        Find Similar Phones <Svg icon={"arrow-right"} />
+        Find Similar Phones >
       </a>
     </div>
   );
