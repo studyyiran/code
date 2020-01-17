511072cb7fca5e28d1c9da61172bf005ca98140c
yiran_aihuishou <yiran.sun@aihuishou.com>
2020-01-17 16:11:59 +0800

    upt-740: 修改视频

--- src/buy/pages/statics/job/components/layout.tsx
+++ src/buy/pages/statics/job/components/layout.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import "./index.less";
 
 interface IProps {}
-export const JobsLayout = (props: IProps) => {
-  return <div className="jobs-layout"></div>;
+export const JobsLayout: React.FC = (props) => {
+  return <div className="jobs-layout">
+    <h3>Jobs Title</h3>
+    <section>
+      {props.children}
+    </section>
+  </div>;
 };
