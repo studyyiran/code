5996be2d68642eb226037bd256def856a0241c57
yiran_aihuishou <yiran.sun@aihuishou.com>
2020-01-17 14:37:23 +0800

    upt-688: 增加json

--- src/buy/pages/detail/ssr.tsx
+++ src/buy/pages/detail/ssr.tsx
@@ -50,7 +50,42 @@ export const detailSsrRule = async (url: string) => {
 
           ssrRes.ssrConfig.metaDesc = `${lineOne} ${lineTwo
             .replace("(", " ")
-            .replace(")", "")}`
+            .replace(")", "")}`;
+          try {
+            const json = {
+              "@context": "https://schema.org/",
+
+              "@type": "Product",
+
+              name: "iPhone 7 256GB Black - Verizon",
+
+              image:
+                "https://d3c745jesl5pj3.cloudfront.net/buy/web/IMG_9632_241b86b0cdfc4d019220119547a700c9.JPG, https://d3c745jesl5pj3.cloudfront.net/buy/web/IMG_9634_9664a55d6308449c89fd5ec6727df35a.JPG, https://d3c745jesl5pj3.cloudfront.net/buy/web/IMG_9636_9481bf8e81404f31bbbfde22a6669608.JPG",
+
+              description:
+                "This used refurbished certified iPhone X 256GB Silver AT&T for sale has passed a rigorous inspection process by UpTrade. View real actual phone photos and inspection report. This phone comes with a 30 day free return policy.",
+
+              brand: "Apple",
+
+              sku: "15807483CR",
+
+              offers: {
+                "@type": "Offer",
+
+                url: "https://uptradeit.com/detail/2193",
+
+                priceCurrency: "USD",
+
+                price: "183",
+
+                availability: "https://schema.org/InStock",
+
+                itemCondition: "https://schema.org/UsedCondition"
+              }
+            };
+            // 制作json数据
+            ssrRes.ssrConfig.jsonInfo = JSON.stringify(json);
+          } catch (e) {}
         }
       }
       const similiarPhoneList: any = await getSimiliar({
