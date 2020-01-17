ea942a2e826b7287f96d4b59ec645df836fd8750
yiran_aihuishou <yiran.sun@aihuishou.com>
2020-01-17 17:48:54 +0800

    upt-740: jobs样式

--- src/buy/pages/statics/job/pages/jobs/index.tsx
+++ src/buy/pages/statics/job/pages/jobs/index.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import "./index.less";
 import "../../common.less";
 import { JobsLayout } from "../../components/layout";
+import RouterLink from "../../../../../common-modules/components/routerLink";
 
 export const JobsPage: React.FC = props => {
   return (
     <JobsLayout className="job-page">
       <ul>
-        <li>He1ads of Operations</li>
-        <li>Operation Intern</li>
+        <li><RouterLink to={'/'}>He1ads of Operations</RouterLink></li>
+        <li><RouterLink to={'/'}>Operation Intern</RouterLink></li>
       </ul>
       <div className="footer">Jan 17, 2019</div>
     </JobsLayout>
