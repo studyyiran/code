dc4410f07efa08f3c8a6aa1c0a4da2e14be9d898
yiran_aihuishou <yiran.sun@aihuishou.com>
2020-01-17 14:48:37 +0800

    upt-688: 增加json

--- src/buy/pages/detail/ssr.tsx
+++ src/buy/pages/detail/ssr.tsx
@@ -37,7 +37,10 @@ export const detailSsrRule = async (url: string) => {
           const {
             brandDisplayName,
             buyProductBQV,
-            productDisplayName
+            productDisplayName,
+            skuId,
+            buyPrice,
+            buyProductImgPc
           } = productDetail as IProductDetail;
           const [lineOne, lineTwo] = getDescArr(
             buyProductBQV,
@@ -54,32 +57,18 @@ export const detailSsrRule = async (url: string) => {
           try {
             const json = {
               "@context": "https://schema.org/",
-
               "@type": "Product",
-
-              name: "iPhone 7 256GB Black - Verizon",
-
-              image:
-                "https://d3c745jesl5pj3.cloudfront.net/buy/web/IMG_9632_241b86b0cdfc4d019220119547a700c9.JPG, https://d3c745jesl5pj3.cloudfront.net/buy/web/IMG_9634_9664a55d6308449c89fd5ec6727df35a.JPG, https://d3c745jesl5pj3.cloudfront.net/buy/web/IMG_9636_9481bf8e81404f31bbbfde22a6669608.JPG",
-
-              description:
-                "This used refurbished certified iPhone X 256GB Silver AT&T for sale has passed a rigorous inspection process by UpTrade. View real actual phone photos and inspection report. This phone comes with a 30 day free return policy.",
-
-              brand: "Apple",
-
-              sku: "15807483CR",
-
+              name: `${lineOne} ${lineTwo.replace("(", "- ").replace(")", "")}`,
+              image: buyProductImgPc,
+              description: ssrRes.ssrConfig.metaDesc,
+              brand: brandDisplayName,
+              sku: skuId,
               offers: {
                 "@type": "Offer",
-
-                url: "https://uptradeit.com/detail/2193",
-
+                url: url,
                 priceCurrency: "USD",
-
-                price: "183",
-
+                price: buyPrice,
                 availability: "https://schema.org/InStock",
-
                 itemCondition: "https://schema.org/UsedCondition"
               }
             };
