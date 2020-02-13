60ff4c013b4313c0c5a8cbf2f6cf6743d6686b43
yiran_aihuishou <yiran.sun@aihuishou.com>
2020-02-13 17:56:54 +0800

    upt-final: upt-776 fix bug

--- src/buy/pages/detail/components/headerProductPart/components/util/index.tsx
+++ src/buy/pages/detail/components/headerProductPart/components/util/index.tsx
@@ -3,29 +3,34 @@ import {IProductDetailGetWithCode} from "../../../../context/interface";
 export const getArrBySort = (productDetailByCode: IProductDetailGetWithCode) => {
   // 首先要排序.
   // 1 先找出quick
-  const findQuickIndex = productDetailByCode.attributes.findIndex(item => {
-    return item.tags === "QUICKFILTERBUY";
-  });
-  let after = [];
-  if (findQuickIndex === -1) {
-    after = [
-      productDetailByCode.condition,
-      ...productDetailByCode.attributes.sort((a, b) => {
-        return a.sort - b.sort;
-      })
-    ];
-  } else {
-    after = [
-      productDetailByCode.attributes[findQuickIndex],
-      productDetailByCode.condition,
-      ...productDetailByCode.attributes
-        .filter((item, index) => {
-          return item.tags !== "QUICKFILTERBUY";
-        })
-        .sort((a, b) => {
+  if (productDetailByCode && productDetailByCode.attributes) {
+    const findQuickIndex = productDetailByCode.attributes.findIndex(item => {
+      return item.tags === "QUICKFILTERBUY";
+    });
+    let after = [];
+    if (findQuickIndex === -1) {
+      after = [
+        productDetailByCode.condition,
+        ...productDetailByCode.attributes.sort((a, b) => {
           return a.sort - b.sort;
         })
-    ];
+      ];
+    } else {
+      after = [
+        productDetailByCode.attributes[findQuickIndex],
+        productDetailByCode.condition,
+        ...productDetailByCode.attributes
+          .filter((item, index) => {
+            return item.tags !== "QUICKFILTERBUY";
+          })
+          .sort((a, b) => {
+            return a.sort - b.sort;
+          })
+      ];
+    }
+    return after
+  } else {
+    return []
   }
-  return after
+
 }
