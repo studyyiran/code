393e7624408f493639cb17ce78939a58facf785c
yiran_aihuishou <yiran.sun@aihuishou.com>
2020-02-18 21:29:56 +0800

    upt-final: 设置弹框样式

--- src/buy/components/header/components/addCart/index.tsx
+++ src/buy/components/header/components/addCart/index.tsx
@@ -1,25 +1,36 @@
 import React, { useContext, useEffect, useState } from "react";
 import "./index.less";
 import { ProductDetailContext } from "../../../../pages/detail/context";
+import { FilterList } from "../../../../pages/productList/components/filsterList";
+import Modal from "../../../modal";
 
 export function AddCart() {
   const productDetailContext = useContext(ProductDetailContext);
   const { productDetailContextValue } = productDetailContext;
   const { cartList } = productDetailContextValue;
   const [listLength, setListLength] = useState(-1);
-  console.log(listLength)
+  const [showModal, setShowModal] = useState(false);
+  console.log(listLength);
+  // 这块没折腾明白
   useEffect(() => {
     // 初始化
-    if (cartList && cartList.length) {
-      console.log('enter')
+    if (cartList) {
+      console.log("enter");
       if (listLength === -1) {
         // 初始化
         setListLength(cartList.length);
       } else {
         // 更新
+        console.log(cartList.length);
+        console.log(listLength);
         if (cartList.length > listLength) {
-          debugger
           // 弹框
+          if (!showModal) {
+            setShowModal(true);
+            setTimeout(() => {
+              setShowModal(false);
+            }, 5000);
+          }
           // 检测到变化后的更新.
           setListLength(cartList.length);
         }
@@ -27,10 +38,26 @@ export function AddCart() {
     }
   }, [cartList, listLength]);
   return (
-    <span className="cart-icon-container">
+    <span className="cart-icon-container" onClick={() => {
+      setShowModal(true);
+    }}>
       <img src={require("./res/cart_icon.png")} />
       <span>Cart</span>
       {listLength > 0 ? <span className="point">{listLength}</span> : null}
+      <Modal
+        visible={showModal}
+        title={""}
+        className="add-cart-modal"
+        maskClosable={true}
+        footer={false}
+        needDefaultScroll={true}
+        closable={false}
+        onCancel={() => {
+          setShowModal(false)
+        }}
+      >
+        <h1>123123</h1>
+      </Modal>
     </span>
   );
 }
