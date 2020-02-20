6f2b755857f8374ae20c0a0f473b5b394ea00f0e
yiran_aihuishou <yiran.sun@aihuishou.com>
2020-02-20 18:14:35 +0800

    upt-final: 增加样式

--- src/buy/pages/shoppingCartPage/index.tsx
+++ src/buy/pages/shoppingCartPage/index.tsx
@@ -6,7 +6,9 @@ import { RenderByCondition } from "../../components/RenderByCondition";
 import Svg from "../../components/svg";
 import Button from "../../components/button";
 import { InnerDivImage } from "../detail/components/innerDivImage";
-import { safeEqual } from "../../common/utils/util";
+import { currencyTrans, safeEqual } from "../../common/utils/util";
+import { OnSaleTag } from "../detail/components/onSaleTag";
+import { getDescArr } from "../detail/util";
 
 export function ShoppingCartPage() {
   // 引入context
@@ -38,28 +40,7 @@ export function ShoppingCartPage() {
         <RenderByCondition
           ComponentPc={
             <li className="list-item">
-              <AddToComparePart
-                buyProductCode={buyProductCode}
-                addCompareList={addCompareList}
-                haveAdded={Boolean(
-                  compareList.find(i => safeEqual(i, buyProductCode))
-                )}
-                imgUrl={
-                  buyProductImgPc && buyProductImgPc.length && buyProductImgPc
-                }
-              />
-              <ProductInfoLine />
-              <CartShoppingItem
-                productDetail={product}
-                partsInfo={skuReleated}
-              />
-              {renderSubTotal()}
-              {renderButton()}
-            </li>
-          }
-          ComponentMb={
-            <li className="list-item">
-              <div>
+              <div className="two-line-flex">
                 <AddToComparePart
                   buyProductCode={buyProductCode}
                   addCompareList={addCompareList}
@@ -70,7 +51,32 @@ export function ShoppingCartPage() {
                     buyProductImgPc && buyProductImgPc.length && buyProductImgPc
                   }
                 />
-                <ProductInfoLine />
+                <div className="content-part">
+                  <ProductInfoLine info={product} />
+                  <CartShoppingItem
+                    productDetail={product}
+                    partsInfo={skuReleated}
+                  />
+                </div>
+              </div>
+              {renderSubTotal()}
+              {renderButton()}
+            </li>
+          }
+          ComponentMb={
+            <li className="list-item">
+              <div className="two-line-flex">
+                <AddToComparePart
+                  buyProductCode={buyProductCode}
+                  addCompareList={addCompareList}
+                  haveAdded={Boolean(
+                    compareList.find(i => safeEqual(i, buyProductCode))
+                  )}
+                  imgUrl={
+                    buyProductImgPc && buyProductImgPc.length && buyProductImgPc
+                  }
+                />
+                <ProductInfoLine info={product} />
               </div>
               <CartShoppingItem
                 productDetail={product}
@@ -95,7 +101,7 @@ export function ShoppingCartPage() {
           <h1>Shopping Cart</h1>
         </div>
         <div>
-          <div>Compare ({123})</div>
+          <div>Compare ({compareList.length})</div>
         </div>
         <ul className="list">{renderList()}</ul>
       </div>
@@ -135,25 +141,68 @@ const AddToComparePart = (props: {
   );
 };
 
-const ProductInfoLine = () => {
+const ProductInfoLine = (props: { info: any }) => {
+  const { info } = props;
+  const {
+    buyProductImgM,
+    buyProductName,
+    buyProductCode,
+    buyProductPrice,
+    buyProductLevel,
+    buyProductId,
+    buyProductBQV,
+    buyProductStatus,
+    buyTags
+  } = info;
+  function renderInfoLine() {
+    const [lineOne, lineTwo] = getDescArr(buyProductBQV, buyProductName);
+    return (
+      <div className="content-container">
+        {lineOne ? (
+          <div className="price-container">
+            <h2>{lineOne}</h2>
+          </div>
+        ) : null}
+        {lineTwo ? <span className="attr">{lineTwo}</span> : null}
+        <span className="condition">Condition {buyProductLevel}</span>
+        <span className="id">Product ID: #{buyProductCode}</span>
+      </div>
+    );
+  }
+
+  function renderRemove() {
+    return <div>Remove</div>;
+  }
   return (
-    <RenderByCondition
-      ComponentPc={
-        <ul className="">
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-        </ul>
-      }
-      ComponentMb={
-        <ul>
-          <li>
-            <div>1</div>
-            <div>3</div>
-          </li>
-          <li>2</li>
-        </ul>
-      }
-    />
+    <ul className="product-info-line">
+      <RenderByCondition
+        ComponentPc={
+          <>
+            <li className="info">{renderInfoLine()}</li>
+            {/*<li>{renderRemove()}</li>*/}
+            <li>
+              <h2 className="price">
+                {currencyTrans(buyProductPrice)}
+                <OnSaleTag tag={buyTags} />
+              </h2>
+            </li>
+          </>
+        }
+        ComponentMb={
+          <>
+            <li>
+              <li>{renderInfoLine()}</li>
+              <div>
+                <h2 className="price">
+                  {currencyTrans(buyProductPrice)}
+                  <OnSaleTag tag={buyTags} />
+                </h2>
+              </div>
+            </li>
+            {/*<li>{renderRemove()}</li>*/}
+          </>
+        }
+      />
+    </ul>
   );
 };
