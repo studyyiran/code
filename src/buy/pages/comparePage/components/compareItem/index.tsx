dd567ead5d535a5a8b8205ebd6a968fb2d64b15b
yiran_aihuishou <yiran.sun@aihuishou.com>
2020-02-24 19:29:17 +0800

    upt-final: 完成棋盘

--- src/buy/pages/comparePage/components/compareItem/index.tsx
+++ src/buy/pages/comparePage/components/compareItem/index.tsx
@@ -23,41 +23,13 @@ export function CompareItem(props: IProps) {
     
   }
   
-  const renderTopPart = () => {
-    return <div>
-      <div>swiper part</div>
-      <button>Buy Now</button>
-      <div>remove</div>
-    </div>
-  }
+ 
   
   // 渲染
   return (
     <li className="compare-item">
-      {isTitleLine ? <div></div> : renderTopPart()}
-      {isTitleLine ? <RenderTitleList config={configArr} /> : <RenderListItem config={configArr} />}
     </li>
   );
 }
 
-const RenderTitleList = (props: { config: any[] }) => {
-  const { config } = props;
-  return (
-    <div>
-      {config.map(({ title }: any) => {
-        return <div>1{title}</div>;
-      })}
-    </div>
-  );
-};
 
-const RenderListItem = (props: { config: any[] }) => {
-  const { config } = props;
-  return (
-    <div>
-      {config.map(({ title }: any) => {
-        return <div>2{title}</div>;
-      })}
-    </div>
-  );
-};
--- src/buy/pages/comparePage/index.tsx
+++ src/buy/pages/comparePage/index.tsx
@@ -2,7 +2,8 @@ import React, { useContext, useEffect } from "react";
 import "./index.less";
 import { locationHref } from "../../common/utils/routerHistory";
 import { StoreShoppingCartContext } from "../shoppingCartPage/context";
-import {CompareItem} from "./components/compareItem";
+import { CompareItem } from "./components/compareItem";
+import { RenderByCondition } from "../../components/RenderByCondition";
 
 interface IProps {
   history?: any;
@@ -18,18 +19,86 @@ export const ComparePage: React.FC<IProps> = props => {
   useEffect(() => {
     getCompareInfoList();
   }, [getCompareInfoList]);
+
+  const renderTopPart = () => {
+    return <div>
+      <div>swiper part</div>
+      <button>Buy Now</button>
+      <div>remove</div>
+    </div>
+  }
+
+  const fillWithEmpty = (maxLength: number) => {
+    const arr = [];
+    for (let i = 0; i < maxLength; i++) {
+      if (compareInfoList && compareInfoList[i]) {
+        arr.push(compareInfoList[i]);
+      } else {
+        arr.push({});
+      }
+    }
+    return arr;
+  };
+
+  const renderPc = (rowInfo: any) => {
+    const { type, title } = rowInfo;
+    const columnArr = [{}].concat(fillWithEmpty(4))
+    return columnArr.map((columnInfo: any, columnIndex: number) => {
+      if (type === 'detail') {
+        if (columnIndex === 0) {
+          return <div />
+        } else {
+          return renderTopPart()
+        }
+      } else {
+        if (columnIndex === 0) {
+          return <RenderTitleList>{title}</RenderTitleList>
+        } else {
+          return <RenderListItem>{title}</RenderListItem>
+        }
+      }
+    })
+  };
+
+  const renderMb = (rowInfo: any) => {
+    const { type, title } = rowInfo;
+  };
+
   function renderList() {
+    const configArr = [
+      {
+        type: "detail",
+        title: "Price"
+      },
+      {
+        type: "",
+        title: "Price"
+      },
+      {
+        type: "",
+        title: "Condition"
+      }
+    ];
+    return configArr.map((rowInfo, rowIndex) => {
+      return (
+        <RenderByCondition
+          ComponentPc={renderPc(rowInfo)}
+          ComponentMb={renderMb(rowInfo)}
+        />
+      );
+    });
     return compareInfoList.map((item, index) => {
       if (true && index === 0) {
-        return <>
-          <CompareItem isTitleLine={true} key={"first"}/>
-          <CompareItem key={index}/>
-        </>
+        return (
+          <>
+            <CompareItem isTitleLine={true} key={"first"} />
+            <CompareItem key={index} />
+          </>
+        );
       } else {
-        return <CompareItem key={index}/>
+        return <CompareItem key={index} />;
       }
-      
-    })
+    });
   }
   return (
     <div className="compare-page">
@@ -37,9 +106,7 @@ export const ComparePage: React.FC<IProps> = props => {
         <h1>Comparison Chart</h1>
       </div>
       <div className="content">
-        <ul className="compare-item-list-container">
-          {renderList()}
-        </ul>
+        <div className="compare-item-list-container">{renderList()}</div>
       </div>
       <div
         className="go-back-button"
@@ -52,4 +119,12 @@ export const ComparePage: React.FC<IProps> = props => {
       </div>
     </div>
   );
+};
+
+const RenderTitleList = ({children}: { children: any }) => {
+  return <div>{children}</div>
+};
+
+const RenderListItem = ({children}: { children: any }) => {
+  return <div>{children}</div>
 };
