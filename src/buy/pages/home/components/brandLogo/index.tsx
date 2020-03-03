95a8c85a6cd7aa84adecf70c0411410666ea95f8
yiran_aihuishou <yiran.sun@aihuishou.com>
2020-03-03 18:59:05 +0800

    upt-test: 修改

--- src/buy/pages/home/components/brandLogo/index.tsx
+++ src/buy/pages/home/components/brandLogo/index.tsx
@@ -27,7 +27,7 @@ export function BrandLogo(props: any) {
     );
   } else {
     return (
-      <div className="comp-brand-logo canclick" onClick={clickHandler}>
+      <div className="comp-brand-logo canclick">
         <div className="border" style={{ border: "none", background: 'none', cursor: 'initial' }}></div>
         <span className="name"></span>
       </div>
